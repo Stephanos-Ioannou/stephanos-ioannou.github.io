b10a3bdf7d5fb7e8fa06466bcbcf672d9e3053f2
Stephanos Ioannou <ioannoustephanos2002@gmail.com>
2025-05-15 23:09:17 +0200

    Migrate from Nextjs to plain html css

--- src/utils/coloredSvg.js
+++
@@ -1,6 +0,0 @@
-const createColoredSvgUrl = (icon, color) => {
-    const modifiedSvg = icon.svg.replace(/<path /g, `<path fill="${color}" `);
-    return `data:image/svg+xml;utf8,${encodeURIComponent(modifiedSvg)}`;
-};
-
-export default createColoredSvgUrl;
